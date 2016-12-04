28abbfec6fc07434d6be0e4e3bd600d176fe91dd
Darren Klein <darren.a.klein@gmail.com>
2016-12-04 11:30:33 -0500

    getting basic index.ejs up and running...

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+	res.render('index', {});
+});
+
+app.listen(port, function(){
+	console.log(`ExpressJS started on port ${port}`);
+});
