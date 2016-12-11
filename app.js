c5dac3f1e08b58273d700879e1161942a3e781fa
Darren Klein <darren.a.klein@gmail.com>
2016-12-11 00:10:46 -0500

    restore sessions

--- app.js
+++ app.js
@@ -75,22 +75,21 @@ app.get('/logout', function(req, res){
 	res.send("Logged out");
 });
 
-// app.use(function(req, res, next) {
-//   if (req.session.userId){
-//     next();
-//     return;
-//   }
-//   res.status(401).send("Please login to view this page.");
-// });
+app.use(function(req, res, next) {
+  if (req.session.userId){
+    next();
+    return;
+  }
+  res.status(401).send("Please login to view this page.");
+});
 
 app.get('/', function(req, res){
 	res.render('index', {});
 });
 
 app.post('/image-upload', upload.single('file-to-upload'), function(req, res, next){
-	//var userId = req.session.userId;
-	var userId = 1;//DELETE THIS
-
+	var userId = req.session.userId;
+	
 	var newImage = {
 		'userId': userId,
 		'original_name': req.file.originalname,
@@ -118,8 +117,7 @@ app.post('/image-upload', upload.single('file-to-upload'), function(req, res, ne
 });
 
 app.get('/get-all', function(req, res){
-	//var userId = req.session.userId;
-	var userId = 1;//DELETE THIS
+	var userId = req.session.userId;
 	models.images.findAll({
 	  include: [{
 	  		model: models.captions
@@ -168,7 +166,6 @@ app.post('/delete-image', function(req, res) {
 
 app.post('/edit-caption', function(req, res){
 	var captionId = req.body.captionId;
-	req.session.userId = 1;//DELETE THIS
 	models.captions.findById(captionId).then(function(row){
 		if (row.dataValues.userId == req.session.userId){
 			models.captions.update({
